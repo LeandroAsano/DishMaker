b2df7c7d1c8fedf6a49064e3f85f4237dfac7c78
Carlos Leandro Asano <leandroasano@Carloss-MBP.glc>
2024-05-03 16:30:46 -0300

    categories test added - Cat Enum
    gitignore files cached
    
    gitignore files cached removed

--- src/main/java/Endpoints/SearchEndpoint.java
+++ src/main/java/Endpoints/SearchEndpoint.java
@@ -3,12 +3,13 @@ package Endpoints;
 import io.restassured.response.Response;
 
 public class SearchEndpoint extends BaseEndpoint {
-    private final String path = "/search.php";
+    private final String PATH = "/search.php";
 
     public SearchEndpoint() {
     }
 
     public Response searchByMealName(String mealName) {
-        return this.baseRequest.queryParam("s", new Object[]{mealName}).get(this.path, new Object[0]).then().log().status().extract().response();
+        return this.baseRequest.request().queryParam("s", new Object[]{mealName})
+                .get(this.PATH);
     }
 }
--- src/test/java/TestFood.java
+++ src/test/java/TestFood.java
@@ -1,4 +1,6 @@
+import Endpoints.ListEndpoint;
 import Endpoints.SearchEndpoint;
+import General.Category;
 import io.restassured.response.Response;
 import jdk.jfr.Description;
 import org.apache.commons.lang3.StringUtils;
@@ -6,8 +8,9 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
+
+import static java.util.Arrays.stream;
 
 @Test
 public class TestFood {
@@ -15,14 +18,29 @@ public class TestFood {
     @Test
     @Parameters({"mealName"})
     @Description("Check for the meal name present in all the endpoint")
-    public void SearchMealByNameTest(String mealName) {
+    public void searchDishByNameTest(String mealName) {
         SearchEndpoint searchEndpoint = new SearchEndpoint();
         Response response = searchEndpoint.searchByMealName(mealName);
-        List<LinkedHashMap> list = response.jsonPath().getList("meals");
-        Assert.assertTrue(list.stream().map((meal) -> {
-            return meal.get("strMeal").toString();
-        }).allMatch((meal) -> {
-            return StringUtils.containsIgnoreCase(meal, mealName);
-        }), "Meal Name: " + mealName + " is present");
+        List<HashMap> list = response.jsonPath().getList("meals");
+        Assert.assertTrue(list.stream()
+                .map((meal) -> meal.get("strMeal").toString())
+                .allMatch((meal) -> StringUtils.containsIgnoreCase(meal, mealName)), "Meal Name: " + mealName + " is present");
     }
+
+    @Test
+    @Description("Check for all enum categories present")
+    public void checkForCategoriesTest(){
+        ListEndpoint listEndpoint = new ListEndpoint();
+        Response response = listEndpoint.listCategories();
+
+        List<HashMap> list = response.jsonPath().getList("meals");
+
+        list.stream()
+                .map( key -> key.get("strCategory"))
+                .map(Object::toString)
+                .forEach( cat -> Assert.assertTrue(Category.isPresent(cat),
+                        "Category " + cat + " not present"));
+
+    }
+
 }
